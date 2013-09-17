37ec145dd6e7a63387fe7b07bb7e019a7baac58f
achim <am@amsoftware.de>
2013-09-18 00:48:59 +0200

    bla

--- MyFirstScalaProject/scripts/hello.scala
+++ MyFirstScalaProject/scripts/hello.scala
@@ -1,1 +1,22 @@
-println ("Hello, World, from a script")
+// 1st approach: Say hello to first argument
+println ("Hello " + args(0) + "!")
+
+// 2nd approach: Show all arguments with while
+println ("2nd approach")
+var i = 0
+while (i<args.length) {
+  if (i!=0)
+    print (" ")
+  print (args(i))
+  i+=1
+}
+println()
+
+// 3rd approach: literal functions
+println ("3rd approach")
+args.foreach(arg => println(arg))
+
+// 4th approach: for
+println ("4th approach")
+for (arg <- args)
+  println (arg)
--- MyFirstScalaProject/src/HelloWorld.scala
+++ MyFirstScalaProject/src/HelloWorld.scala
@@ -1,7 +1,31 @@
 object HelloWorld {
 
-  def main(args: Array[String]): Unit = {
-    println("Hello, World!");
+	def main(args: Array[String]): Unit = {
+    
+    println("Hello, World!")
+    
+    // A Function literal
+    args.foreach( x => println(x))
+ 
+    // Arrays
+    val greetStrings = new Array[String](3)
+    // Kurzform von...
+    val greetStrings2 : Array[String] = new Array[String](3);
+    // () wird übersetzt nach .apply
+    val greetStrings3 = Array.apply(3)
+    // kann man auch für init benutzen
+    val greetStrings4 = Array.apply("Scala", "ist", "cool")
+    
+    greetStrings(0) = "Scala"
+    greetStrings(1) = "ist"
+    // = wird transformiert zu .update
+    greetStrings.update(2, "cool")
+    
+    for (i <- 0 to 2) {
+    	println (greetStrings(i));
+    	// Kurzform von...
+    	println (greetStrings.apply(i));
+      }
   }
-
+  
 }
